29d8232b52d7730f46f8d16888bc5199222a9837
bota21 <aimkulow@bk.ru>
2021-05-06 02:57:06 +0600

    #7 Реализован выход из сессии пользователя

--- app/users.js
+++ app/users.js
@@ -21,15 +21,31 @@ const createRouter = () => {
     if (!user) {
       return res.status(400).send("Wrong username or password");
     }
- 
+
     const isMatch = await user.checkPassword(req.body.password);
 
     if (!isMatch) {
       return res.status(400).send("Wrong username or password");
     }
     user.generateToken();
-    await user.save({validateBeforeSave: false});
+    await user.save({ validateBeforeSave: false });
     return res.send(user);
+  });
+
+  router.delete("/sessions", async (req, res) => {
+    const token = req.get("Authentication");
+    const success = { message: "Success" };
+
+    if (!token) return res.send(success);
+
+    const user = await Users.findOne({ token });
+
+    if (!user) return res.send(success);
+
+    user.token = user.generateToken();
+    user.save({ validateBeforeSave: false });
+
+    return res.send(success);
   });
 
   return router;
